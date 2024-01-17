54d6335041c71d134857a6c3f4894dae7956cf01
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-01-17 01:45:10 +0100

    fianlisation de la methode modifier un produit

--- src/app/update-produit/update-produit.component.ts
+++ src/app/update-produit/update-produit.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-update-produit',
   templateUrl: './update-produit.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class UpdateProduitComponent implements OnInit {
+  currentProduit = new Produit();
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private produitService: ProduitService) { }
 
   ngOnInit(): void {
+    console.log(this.activatedRoute.snapshot.params['id']);
+    this.currentProduit = this.produitService.consulterProduit(this.activatedRoute.snapshot.params['id']);
+    console.log(this.currentProduit);
+  }
+
+  //methode update produit
+  updateProduit(){
+    //console.log(this.currentProduit);
+    this.produitService.updateProduit(this.currentProduit)
   }
 
 }
