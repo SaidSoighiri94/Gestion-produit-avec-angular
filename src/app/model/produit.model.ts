9db9334c19d416e588a677ca66cc1021b54b68a4
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-03-07 11:44:44 +0100

    ajout d'une methode permettant le tri des prodits ainsi et la categorie des produits

--- src/app/model/produit.model.ts
+++ src/app/model/produit.model.ts
@@ -1,7 +1,10 @@
+import { Categorie } from "./categorie.model";
+
 //  Creation de la classe produit dans notre model 
 export class Produit {
     idProduit?: number;
     nomProduit?: string;
     prixProduit?: number;
     dateCreation?: Date;
+    categorie! : Categorie;
 }
--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Produit } from '../model/produit.model';
+import { Categorie } from '../model/categorie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitService {
-  produits: Produit[]; //un tableau de Produi
+  produits: Produit[]; //un tableau de Produits
   produit!: Produit;
+  categories : Categorie[];  // un Tableau de categorie 
   constructor() {
+    this.categories = [
+      {idCat : 1, nomCat: "Pc"},
+      {idCat: 1, nomCat : "Imprimante"}
+    ];
+
     this.produits = [
-      { idProduit: 1, nomProduit: "Pc Asus", prixProduit: 4600, dateCreation: new Date("12-23-2023") },
-      { idProduit: 2, nomProduit: "Samsung Tablette", prixProduit: 2333, dateCreation: new Date("11/16/2020") },
-      { idProduit: 3, nomProduit: "Imprimante Epson", prixProduit: 450, dateCreation: new Date("12/17/2010") }
-    ]
+      { idProduit: 1, nomProduit: "Pc Asus", prixProduit: 4600, dateCreation: new Date("12-23-2023"),
+      categorie : {idCat : 1, nomCat : "PC"}},
+      { idProduit: 2, nomProduit: "Samsung Tablette", prixProduit: 2333, dateCreation: new Date("11/16/2020"),
+      categorie : {idCat :1,nomCat : "Pc"} },
+      { idProduit: 3, nomProduit: "Imprimante Epson", prixProduit: 450, dateCreation: new Date("12/17/2010"), 
+      categorie: {idCat : 2, nomCat : "Imprimante"} }
+    ];
   }
 
   // on va creer une methode qui va qui va nous retourner un tablea de produit
@@ -40,5 +50,21 @@ export class ProduitService {
   updateProduit(p: Produit) {
     this.supprimerProduit(p);
     this.ajouterProduit(p);
+    this.trierProduits();
+    
+  }
+
+  trierProduits(){                        // Ajout d'une methode de tri des produits
+    this.produits =this.produits.sort((n1,n2) =>{
+      if (n1.idProduit! > n2.idProduit!) {
+        return 1
+      }
+      if (n1.idProduit! < n2.idProduit!) {
+        return -1
+      }
+      return 0;
+    });
+
+
   }
 }
