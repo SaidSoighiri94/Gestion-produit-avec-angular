4ac46f854d902c4f1e777247268a1ed0c3de5ece
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-07-24 17:18:18 +0200

    Implementation de methode de verification du  guard

--- src/app/produit.guard.ts
+++ src/app/produit.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitGuard implements CanActivate {
+
+  //Ajout d'un constructeur suivi d'une injection de dependance 
+  constructor(private authService: AuthService, private router :Router) {
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): boolean{
+    // verification si l'utilisateur est un admin
+    if(this.authService.isAdmin())
+      return true;
+    else {
+      this.router.navigate(['app-forbidden']);
+      return false;
+    }
   }
-  
+
 }
