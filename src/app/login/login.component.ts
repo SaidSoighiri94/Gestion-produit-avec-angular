7995e2bc764d750291e9d068a9e9b3cba0c13091
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-10-07 23:09:27 +0200

    Modification de la methode onLoggiedIn  du loginComponent.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,20 +11,20 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   user = new User();
-  erreur =0;
+  err:number = 0;
 
   constructor(private authService :AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
   onLoggedIn(){
-    console.log(this.user);
-    let isValidUser: Boolean = this.authService.SignIn(this.user);
-    if(isValidUser)
-    this.router.navigate(['/']);
-    else
-    this.erreur =1;
+    this.authService.login(this.user).subscribe((data)=> {
+      let jwToken = data.headers.get('Authorization')!;
+      this.authService.saveToken(jwToken);
+      this.router.navigate(['/']);
+    },(erreur) =>{this.err = 1;
 
+    });
   }
 
 }
